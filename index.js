a7cf8f82137800c1c55da5f602de9603d3a85677
Albert Villaruz <Albert.Villaruz@GeorgianCollege.ca>
2018-05-02 15:28:38 -0400

    index.js text/html

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((request, response) => {
   // send a response to the browser
   response.statusCode = 200; // sending http status code ok
-  response.setHeader('Content-Type', 'text/plain');
+  response.setHeader('Content-Type', 'text/html');
   response.write('<h1>our first node page</h1>');
   response.end('<h2>Ending the response</h2>');
 });
