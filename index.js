af1213b24ec525e945cfd7d5915ac24cff388339
Albert Villaruz <Albert.Villaruz@GeorgianCollege.ca>
2018-05-02 15:27:25 -0400

    index.js statuscode

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const http = require('http');
 const PORT = process.env.PORT || 5000;
 
 // start the web server
-http
-  .createServer((request, response) => {
-    // send a response to the browser
-    response.statusCode(200); // sending http status code ok
-    response.setHeader('Content-Type', 'text/plain');
-    response.write('<h1>our first node page</h1>');
-    response.end('<h2>Ending the response</h2>');
-  })
-  .listen(PORT, () => {
-    console.log(`Server running on ${PORT}/`);
-  });
+const server = http.createServer((request, response) => {
+  // send a response to the browser
+  response.statusCode = 200; // sending http status code ok
+  response.setHeader('Content-Type', 'text/plain');
+  response.write('<h1>our first node page</h1>');
+  response.end('<h2>Ending the response</h2>');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
+});
