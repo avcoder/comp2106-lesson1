629d8c1e4fdea7795e5a63cbab6134b0b207bc83
Albert Villaruz <Albert.Villaruz@GeorgianCollege.ca>
2018-05-02 15:43:20 -0400

    index.js see if i can break build

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const http = require('http');
 
-const PORT = process.env.PORT || 5000;
-
 // start the web server
-const server = http.createServer((request, response) => {
-  // send a response to the browser
-  response.statusCode = 200; // sending http status code ok
-  response.setHeader('Content-Type', 'text/plain');
-  response.write('<h1>our first node page</h1>');
-  response.end('<h2>Ending the response</h2>');
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
-});
+const server = http
+  .createServer((request, response) => {
+    // send a response to the browser
+    response.statusCode = 200; // sending http status code ok
+    response.setHeader('Content-Type', 'text/plain');
+    response.write('<h1>our first node page</h1>');
+    response.end('<h2>Ending the response</h2>');
+  })
+  .listen(process.env.PORT, () => {
+    console.log('Server running');
+  });
